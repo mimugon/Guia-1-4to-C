73f03f67612720f87caf495481d7e7ac9ed37f8b
mimugon <49438383@est.ort.edu.ar>
2025-04-03 11:40:56 -0300

    Guia 1 terminada

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+
+
+{
+    public string nombreUsuario;
+    public int edadUsuario;
+    public float alturaUsuario;
+    public bool esDonante;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("mi nombre es " + nombreUsuario + " , tengo " + edadUsuario + " ,mido " + alturaUsuario + " y es " + esDonante +" que soy donante");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EjercicioCalculos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjercicioCalculos : MonoBehaviour
+{
+
+    public float num1;
+    public float num2;
+    private float resultadoSuma;
+    private float resultadoResta;
+    private float resultadoDivision;
+    private float resultadoMult;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        resultadoSuma = num1 + num2;
+        Debug.Log(resultadoSuma);
+
+        resultadoResta = num1 - num2;
+        Debug.Log(resultadoResta);
+
+        resultadoDivision = num1 / num2;
+        Debug.Log(resultadoDivision);
+
+        resultadoMult = num1 * num2;
+        Debug.Log(resultadoMult);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
